419642808ab2609e7a6cd6fa5016a35dcd7e0285
m.osipov <mark.osipov@gmail.com>
2023-05-02 06:33:54 +0400

    Log request and response Log to json file

--- qa_hub_backend/src/main/kotlin/qa_hub/config/ErrorHandler.kt
+++ qa_hub_backend/src/main/kotlin/qa_hub/config/ErrorHandler.kt
@@ -1,4 +1,4 @@
-package qa_hub.core
+package qa_hub.config
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+
 
 @ControllerAdvice
 class ErrorHandler {
@@ -19,7 +20,12 @@ class ErrorHandler {
         e: java.lang.Exception?,
         request: HttpServletRequest?, response: HttpServletResponse?
     ): ResponseEntity<ErrorResponse> {
+        val builder = StringBuilder()
+        builder.append(e?.stackTrace?.joinToString("\n   "))
+
         logger.error(e?.message)
+        logger.error(builder.toString()) //For json logger
+
         e?.printStackTrace()
         return ResponseEntity.internalServerError().body(ErrorResponse(message = e?.message))
     }
---
+++ qa_hub_backend/src/main/kotlin/qa_hub/core/utils/StringUtils.kt
@@ -0,0 +1,20 @@
+package qa_hub.core.utils
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
+
+
+fun String.toPrettyJson(): String {
+    try {
+        val parser = JsonParser()
+        val json: JsonElement = if (this.trim().first() == '[') {
+            parser.parse(this).asJsonArray
+        } else { parser.parse(this).asJsonObject }
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        return gson.toJson(json)
+    } catch (e: Exception) {
+        throw Exception("Failed to prettify json. Source string is not valid json: $this")
+    }
+}
