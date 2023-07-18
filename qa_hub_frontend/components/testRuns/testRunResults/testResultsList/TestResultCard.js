ffca368012f0c9e5c0e371a4c90203d84e5a7d59
m.osipov <mark.osipov@gmail.com>
2023-07-19 02:14:18 +0400

    Add box shadow for test result card

--- qa_hub_frontend/components/testRuns/testRunResults/testResultsList/TestResultCard.js
+++ qa_hub_frontend/components/testRuns/testRunResults/testResultsList/TestResultCard.js
@@ -24,6 +24,8 @@ export default function TestResultCard({testResult, setSelectedTest, ...props}) 
       display: 'flex',
       backgroundColor: isHovering ? 'rgba(255, 255, 255, 0.09)' : 'rgba(255, 255, 255, 0.05)',
       cursor: 'pointer',
+      border: '1px solid rgba(0, 0, 0, 0.3)',
+      boxShadow: '2px 2px 0 rgba(0, 0, 0, 0.25)',
       ...props.style
   }}
     onMouseEnter={onMouseEnter}
