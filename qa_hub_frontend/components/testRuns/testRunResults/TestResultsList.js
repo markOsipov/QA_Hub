f5c113ccc66e20b1c8e523be9017413bc794b862
m.osipov <mark.osipov@gmail.com>
2023-07-18 17:48:10 +0400

    Test results list fixes

--- qa_hub_frontend/components/testRuns/testRunResults/TestResultsList.js
+++ qa_hub_frontend/components/testRuns/testRunResults/TestResultsList.js
@@ -3,11 +3,24 @@ import StatusBadge from "../../primitives/StatusBadge";
 import TextWithLabel from "../../primitives/TextWithLabel";
 import Typography from "@mui/material/Typography";
 import TestResultCard from "./TestResultCard";
+import {useEffect, useState} from "react";
+import useSWR from "swr";
+import {getTestResults} from "../../../requests/TestResultsRequests";
 
-export default function TestResultsList({loader, testResults, setSelectedTest, ...props }) {
-  if (loader) {
-    return <Paper style={{padding: '15px', ...props.style}}>Loading test results</Paper>
-  }
+export default function TestResultsList({testRunId, setSelectedTest, ...props }) {
+  const [testResults, setTestResults] = useState([])
+
+  let testResultsResp = useSWR(
+    "getTestResults",
+    async () => { return await getTestResults(testRunId) },
+    { refreshInterval: 60000 }
+  )
+
+  useEffect(() => {
+    if (testResultsResp.data?.data) {
+      setTestResults(testResultsResp.data.data)
+    }
+  }, [testResultsResp.data])
 
   return <Paper style={{padding: '15px', ...props.style}}>
     <label>Test results list</label>
--- qa_hub_frontend/pages/testRuns/[testRunId].js
+++ qa_hub_frontend/pages/testRuns/[testRunId].js
@@ -13,29 +13,13 @@ export default function TestRunPage() {
   const testRunId = router.query.testRunId
 
   const [testRun, setTestRun] = useState(null)
-  const [testResults, setTestResults] = useState([])
-  const [testResultsLoader, setTestResultsLoader] = useState(false)
   const [selectedTest, setSelectedTest] = useState(null)
 
   let testRunResp = useSWR(
     "getTestRun",
     async () => { return await getTestRun(testRunId) },
-    { refreshInterval: 60000, revalidateOnFocus: false, revalidateIfStale: false }
+    { refreshInterval: 60000 }
   )
-
-  let testResultsResp = useSWR(
-    "getTestResults",
-    async () => { return await updateTestResults() },
-    { refreshInterval: 60000, revalidateOnFocus: false, revalidateIfStale: false }
-  )
-
-  const updateTestResults = async () => {
-    setTestResultsLoader(true)
-    const resp = await getTestResults(testRunId)
-    setTestResultsLoader(false)
-
-    return resp
-  }
 
   useEffect(() => {
     if (testRunResp.data?.data) {
@@ -43,30 +27,21 @@ export default function TestRunPage() {
     }
   }, [testRunResp.data])
 
-  useEffect(() => {
-    if (testResultsResp.data?.data) {
-      setTestResults(testResultsResp.data.data)
-    }
-  }, [testResultsResp.data])
 
   return <div style={{padding: "15px"}}>
     <TestRunResultsOverview testRun={testRun}/>
 
     <div style={{display: "flex", marginTop: '15px', width: '100%', minWidth: '100%'}}>
       <TestResultsList
-        loader={testResultsLoader}
-        testResults={testResults}
-        style={{width: "26%", minWidth: '500px', maxWidth: '70%', overflowX: 'auto', resize: 'horizontal'}}
+        testRunId={testRunId}
         setSelectedTest={setSelectedTest}
+        style={{width: "26%", minWidth: '500px', maxWidth: '70%', overflowX: 'auto', resize: 'horizontal'}}
       />
       <TestResultDetails testResult={selectedTest} style={{flexGrow: '1.1', marginLeft: '15px'}}></TestResultDetails>
     </div>
 
     <Paper style={{padding: "10px", marginTop: "15px"}}>
       {JSON.stringify(testRun)}
-    </Paper>
-    <Paper  style={{padding: "10px", marginTop: "15px"}}>
-      {JSON.stringify(testResults)}
     </Paper>
   </div>
 }
