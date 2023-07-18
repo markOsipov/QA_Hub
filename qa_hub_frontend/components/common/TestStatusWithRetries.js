0385a391fc27c0563040c2715350b71c40e38d1b
m.osipov <mark.osipov@gmail.com>
2023-07-19 02:00:03 +0400

    Retries indicator

--- qa_hub_frontend/components/common/TestStatusWithRetries.js
+++ qa_hub_frontend/components/common/TestStatusWithRetries.js
@@ -2,48 +2,19 @@ import StatusBadge from "../primitives/StatusBadge";
 import {customTheme} from "../../styles/CustomTheme";
 
 export default function TestStatusWithRetries({status, retries, ...props}) {
-  const borderColor1 = 'rgba(0, 0, 0, 1)'
-  const borderColor = 'rgba(255, 255, 255, 0.9)'
-  const NumericRetriesBubble = () => {
-    return <div
-      style={{
-        position: 'absolute',
-        borderRadius: '10px',
-        border: '2px solid',
-        borderColor: borderColor,
-        backgroundColor: status == 'SUCCESS' ? customTheme.palette.success.main : customTheme.palette.error.main,
-        width: '18px',
-        height: '18px',
-        justifySelf: 'center',
-        display: 'grid',
-        top: '16px'
-      }}
-    >
-      <label
-        style={{
-          justifySelf: "center",
-          alignSelf: 'center',
-          position: "relative",
-          left: '0px',
-          top: '-1px',
-          fontSize:'13px',
-          fontWeight: '500',
-          color: 'white'
-        }}
-      >{retries}</label>
-    </div>
-  }
-
   const VisualRetriesBubble = () => {
-    const bubbleSize = '9px'
+    const bubbleSize = '8px'
     return <div
       style={{
         display: 'flex',
         justifySelf: "center",
         alignSelf: 'center',
         position: "relative",
-        top: '-4px',
-        opacity: '0.99'
+        top: '-6px',
+        padding: '3px 3px',
+        backgroundColor: 'rgb(0, 0, 0)' ,
+        borderRadius: '8px',
+        border: '1px solid rgba(255, 255, 255, 0.55)'
       }}
     >
       {
@@ -52,9 +23,9 @@ export default function TestStatusWithRetries({status, retries, ...props}) {
             height: bubbleSize,
             width: bubbleSize,
             borderRadius: '5px',
-            border: `1px solid`,
-            borderColor: borderColor,
+            border: '1px solid rgba(255, 255, 255, 0.8)',
             marginLeft: '1px',
+            marginRight: '1px',
             backgroundColor:  status === 'SUCCESS' && index === retries - 1 ? customTheme.palette.success.main : customTheme.palette.error.main,
           }} />
         })
@@ -62,18 +33,10 @@ export default function TestStatusWithRetries({status, retries, ...props}) {
     </div>
   }
 
-  function getRetriesIndicator() {
-    if (retries > 5) {
-      return <NumericRetriesBubble />
-    }
-    return <VisualRetriesBubble />
-  }
 
-  return <div style={{marginLeft: '4px', position: 'relative', display: 'grid'}}>
+  return <div style={{marginLeft: '4px', position: 'relative', display: 'grid', ...props.style}}>
     <StatusBadge label={status}/>
-    {
-      getRetriesIndicator()
-    }
+    <VisualRetriesBubble />
   </div>
 
 
