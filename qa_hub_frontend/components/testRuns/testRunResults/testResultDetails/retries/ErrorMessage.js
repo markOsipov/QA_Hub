5a7fb332ec3332d4b633164a0feba546fb2e79d5
m.osipov <mark.osipov@gmail.com>
2023-07-20 18:00:46 +0400

    Error message selection handler fix

--- qa_hub_frontend/components/testRuns/testRunResults/testResultDetails/retries/ErrorMessage.js
+++ qa_hub_frontend/components/testRuns/testRunResults/testResultDetails/retries/ErrorMessage.js
@@ -1,14 +1,26 @@
 import {customTheme} from "../../../../../styles/CustomTheme";
 import {createRef, useEffect, useRef, useState} from "react";
+import Button from "@mui/material/Button";
 
 export default function ErrorMessage({ message, ...props }) {
   const ref = createRef()
+  const [anchorEl, setAnchorEl] = useState(null)
+
+  const [selectedText, setSelectedText] = useState(null)
   const handleSelect = () => {
     const selection = window.getSelection()
-    if (!selection.toString()) return;
-    const selectedText = selection.extentNode.textContent.slice(selection.anchorOffset, selection.focusOffset)
+    const { current: el } = ref
 
-    console.log(selectedText)
+    if (!selection.toString()) return;
+
+    if (selection.anchorNode.parentNode === ref.current || selection.focusNode.parentNode === ref.current) {
+      const selected = selection.extentNode.textContent.slice(selection.anchorOffset, selection.focusOffset)
+      setSelectedText(selected)
+    }
+  }
+
+  const closeMenu = () => {
+    setAnchorEl(null)
   }
 
   useEffect(() => {
@@ -21,27 +33,24 @@ export default function ErrorMessage({ message, ...props }) {
     }
   }, [handleSelect])
 
-
-
   if (!message) return null
 
-  return  <div
-    rootRef={ref}
-    style={{
-      padding: '10px',
-      border: '1px solid',
-      borderColor: customTheme.palette.error.main,
-      borderRadius: '10px',
-      backgroundColor: customTheme.palette.error.faded,
-      width: 'max-content',
+  return <div onBlur={() => { setSelectedText(null) }}>
+    <div
+      style={{
+        padding: '10px',
+        border: '1px solid',
+        borderColor: customTheme.palette.error.main,
+        borderRadius: '10px',
+        backgroundColor: customTheme.palette.error.faded,
+        width: 'max-content',
 
-      display: 'grid',
-      ...props.style
-    }}>
-      {
-        message.split("\n").map((line, index) => {
-          return <label key={index} style={{whiteSpace: 'break-spaces'}}>{line}</label>
-        })
-      }
+        display: 'grid',
+        ...props.style
+      }}
+    >
+      <label ref={ref} style={{whiteSpace: 'break-spaces'}}
+      >{message}</label>
     </div>
+  </div>
 }
