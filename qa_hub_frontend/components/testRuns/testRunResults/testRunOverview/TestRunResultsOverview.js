61fd4b5c8516e750696306ef7a0a089bde07e22a
m.osipov <mark.osipov@gmail.com>
2023-07-24 00:57:42 +0400

    Return testrun overview card

--- qa_hub_frontend/components/testRuns/testRunResults/testRunOverview/TestRunResultsOverview.js
+++ qa_hub_frontend/components/testRuns/testRunResults/testRunOverview/TestRunResultsOverview.js
@@ -6,7 +6,6 @@ import TimingsPlate from "../../testRunList/testRun/elements/TimingsPlate";
 import RunnersPlate from "../../testRunList/testRun/elements/RunnersPlate";
 import GitEnvPlate from "../../testRunList/testRun/elements/GitEnvPlate";
 import RunnersDetailsPlate from "./RunnersDetailsPlate";
-import TestRunCard from "../../testRunList/testRun/TestRunCard";
 
 export default function TestRunResultsOverview({testRun, ...props}) {
   if (testRun == null) {
@@ -14,10 +13,45 @@ export default function TestRunResultsOverview({testRun, ...props}) {
   }
   return <Paper style={{padding: '15px 20px', minWidth: 'max-content', ...props.style}}>
     <div style={{display: 'flex', flexDirection: 'column'}}>
-      <TestRunCard testRun={testRun}></TestRunCard>
+      <div style={{display: 'flex'}}>
+        <div>
+          <div style={{ marginTop: "10px", display: "flex", alignItems: 'center' }}>
+            <TextWithLabel
+              value={testRun.testRunId}
+              label={'testRunId'}
+              labelStyle={{ justifySelf: 'center'}}
+              style={{fontSize: "17px", width: "min-content", padding: "5px 6px", minHeight: 'unset'}}
+            />
+            <StatusBadge label={testRun.status} style={{ marginLeft: "10px"}}/>
+          </div>
+          <TestResultsPlate testRun={testRun} style={{marginTop: '55px'}}/>
 
+          <div style={{display: 'flex', marginTop: '35px'}}>
+            <GitEnvPlate testRun={testRun}  style={{minWidth: '300px'}}/>
+            <RunnersPlate testRun={testRun} style={{minWidth: '300px', marginLeft: '35px'}} />
+          </div>
 
-      <RunnersDetailsPlate testRun={testRun} style={{marginLeft: '50px', marginTop: '65px'}} />
+          {
+            testRun.timeMetrics != null &&
+            <TimingsPlate
+              testRun={testRun}
+              style={{
+                display: 'grid',
+                alignItems: 'end',
+                justifyItems: 'start',
+                minWidth: '370px',
+                position: 'relative',
+                marginTop: '55px'
+              }}
+            />
+          }
+        </div>
+
+        {
+          testRun.runners.length > 0 &&
+          <RunnersDetailsPlate testRun={testRun} style={{marginLeft: '50px', marginTop: '65px'}} />
+        }
+      </div>
     </div>
   </Paper>
 }
