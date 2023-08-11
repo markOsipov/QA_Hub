45a0d128bdf2115e92e3b106e9a524bde310cb24
m.osipov <mark.osipov@gmail.com>
2023-08-11 12:09:09 +0400

    Changed deployscript and mongo instance creation

--- qa_hub_backend/src/main/kotlin/qa_hub/core/mongo/QaHubMongoClient.kt
+++ qa_hub_backend/src/main/kotlin/qa_hub/core/mongo/QaHubMongoClient.kt
@@ -2,6 +2,7 @@ package qa_hub.core.mongo
 
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
+import com.mongodb.MongoCredential
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
@@ -11,25 +12,25 @@ import org.springframework.stereotype.Repository
 class QaHubMongoClient {
     private val qaHubDb = "dbQaHub"
 
-    private val mongoHost = "mongodb://localhost:27017"  //System.getenv("ENV_MONGO_QA_HUB_HOST")
-
-//    private val mongoHost = System.getenv("ENV_MONGO_QA_HUB_HOST")
-//    private val userName = System.getenv("ENV_MONGO_QA_HUB_LOGIN")
-//    private val userPass = System.getenv("ENV_MONGO_QA_HUB_PASSWORD")
+    private val mongoHost = System.getenv("ENV_MONGO_QA_HUB_HOST")
+    private val userName = System.getenv("ENV_MONGO_QA_HUB_LOGIN")
+    private val userPass = System.getenv("ENV_MONGO_QA_HUB_PASSWORD")
+    private val authSource = System.getenv("ENV_MONGO_QA_HUB_AUTH_SOURCE")
 
     val client = run {
-        KMongo.createClient(
-                MongoClientSettings.builder()
-                        .applyConnectionString(ConnectionString(mongoHost))
-//                        .credential(
-//                                MongoCredential.createScramSha1Credential(
-//                                        userName,
-//                                        "admin",
-//                                        userPass.toCharArray()
-//                                )
-//                        )
-                        .build()
-        )
+        val builder = MongoClientSettings.builder()
+        builder.applyConnectionString(ConnectionString(mongoHost))
+
+        if (!userName.isNullOrEmpty() && userPass.isNullOrEmpty()) {
+            builder.credential(
+                MongoCredential.createScramSha1Credential(
+                    userName,
+                    authSource ?: "admin",
+                    userPass.toCharArray()
+                )
+            )
+        }
+        KMongo.createClient(builder.build())
     }
 
     val db: CoroutineDatabase = client.coroutine.getDatabase(qaHubDb)
