a9a18ce0f82351e22e6a02cb84313abfc98c70e7
m.osipov <mark.osipov@gmail.com>
2023-08-22 18:26:45 +0400

    Fix rerun

--- qa_hub_backend/src/main/kotlin/qa_hub/service/TestRunService.kt
+++ qa_hub_backend/src/main/kotlin/qa_hub/service/TestRunService.kt
@@ -105,7 +105,7 @@ class TestRunService {
 
         testRunCollection.aggregate<TestRun>(query).toList()
     }
-    fun createTestRun(testRunRequest: CreateTestRunRequest): TestRun = runBlocking {
+    fun createTestRun(testRunRequest: CreateTestRunRequest, startJob: Boolean = true): TestRun = runBlocking {
         val testRun = TestRun(
             testRunId = currentEpoch().toString(),
             project = testRunRequest.project,
@@ -118,12 +118,18 @@ class TestRunService {
 
         testRunCollection.insertOne(testRun)
 
-        return@runBlocking getTestRun(testRun.testRunId)!!
+        val insertedTestRun = getTestRun(testRun.testRunId)!!
+
+        if (startJob) {
+            startJob(insertedTestRun, testRunRequest.branch)
+        }
+
+        return@runBlocking insertedTestRun
     }
 
     fun startTestRun(startTestRunRequest: StartTestRunRequest) = runBlocking {
         val testRun: TestRun = if (startTestRunRequest.testRunId.isNullOrBlank()) {
-            createTestRun(startTestRunRequest)
+            createTestRun(startTestRunRequest, false)
         } else {
             getTestRun(startTestRunRequest.testRunId!!)!!
         }
@@ -391,13 +397,11 @@ class TestRunService {
     fun startRerun(testRunId: String) = runBlocking {
         val testRun = testRunCollection.findOne(TestRun::testRunId eq testRunId)!!
 
-        val newTestRun = createTestRun(CreateTestRunRequest(
+        createTestRun(CreateTestRunRequest(
             testRun.project,
             testRun.config!!.branch,
             testRun.params
         ))
-
-        startJob(newTestRun, newTestRun.config!!.branch)
     }
 
     fun deleteTestRun(testRunId: String) = runBlocking {
