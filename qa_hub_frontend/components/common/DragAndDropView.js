dbe426ab3067a208e8d5f12ad267c372d8c7109d
m.osipov <mark.osipov@gmail.com>
2023-09-12 18:33:50 +0400

    Drag and drop view

--- qa_hub_frontend/components/common/DragAndDropView.js
+++ qa_hub_frontend/components/common/DragAndDropView.js
@@ -6,6 +6,7 @@ import StyledTooltip from "../primitives/StyledTooltip";
 
 const DragAndDropView = observer(({initialX, initialY, ...props}) => {
   const dragRef = useRef()
+  const contentRef = useRef()
   const [isDragging, setIsDragging] = useState(false)
 
   const [mouseCoordinates, setMouseCoordinates] = useState({x:0, y:0});
@@ -13,12 +14,17 @@ const DragAndDropView = observer(({initialX, initialY, ...props}) => {
     x: initialX || 50,
     y:initialY || 50
   })
+
+  const [elementHeight, setElementHeight] = useState(0)
+  const [elementWidth, setElementWidth] = useState(0)
   const startDragging = () => {
     setIsDragging(true)
+    window.addEventListener('mousemove', mouseMoveHandler);
   }
 
   const stopDragging = () => {
     setIsDragging(false)
+    window.removeEventListener('mousemove', mouseMoveHandler);
   }
 
   const mouseMoveHandler = (event) => {
@@ -29,31 +35,49 @@ const DragAndDropView = observer(({initialX, initialY, ...props}) => {
   }
 
   useEffect(() => {
+    if (contentRef?.current?.clientWidth) {
+      setElementWidth(contentRef.current.clientWidth)
+    }
+    if (contentRef?.current?.clientHeight) {
+      setElementHeight(contentRef.current.clientHeight)
+    }
+  }, [contentRef?.current?.clientWidth, contentRef?.current?.clientHeight])
+
+  useEffect(() => {
+    const minX = 5
+    const minY = 5
+    const maxX = window.innerWidth - elementWidth - 10
+    const maxY = window.innerHeight - elementHeight - 10
+    const mouseX =  mouseCoordinates.x
+    const mouseY =  mouseCoordinates.y
+
     if (isDragging) {
       setElementCoords({
-        x: mouseCoordinates.x,
-        y: mouseCoordinates.y
+        x: Math.max(Math.min(mouseX, maxX), minX),
+        y: Math.max(Math.min(mouseY, maxY), minY)
       })
     }
   }, [mouseCoordinates.x, mouseCoordinates.y])
 
-  useEffect(()=>{
-    window.addEventListener('mousemove', mouseMoveHandler);
-    return(()=>{
-      window.removeEventListener('mousemove', mouseMoveHandler);
-    })
-  }, [])
+  // useEffect(()=>{
+  //   window.addEventListener('mousemove', mouseMoveHandler);
+  //   return(()=>{
+  //     window.removeEventListener('mousemove', mouseMoveHandler);
+  //   })
+  // }, [])
 
   return <div
     ref={dragRef}
-
     style={{
       position: 'absolute',
       left: elementCoords.x && elementCoords.x + 'px',
       top: elementCoords.y && elementCoords.y + 'px',
     }}
+    onMouseUp={stopDragging}
+    onBlur={stopDragging}
   >
     <div
+      ref={contentRef}
       style={{ position: 'relative', ...props.style }}
     >
       {
